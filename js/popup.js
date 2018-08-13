d23bb7d2cea438bbd855eb405caad38444642ce6
Waqas Hassan <waqashsn@yahoo.com>
2018-08-13 12:51:41 +0500

    Added functionality to check if current page is a Wikipedia article or not

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,24 @@
-//Javascript code for the extension popup goes here
+//get the read article btn
+let readBtn = document.getElementById("read_article");
+
+readBtn.onclick = function() {
+  //get currently active tab in browser window
+  browser.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    //tabs object contains info about active tab
+
+    //get the full url of the tab
+    var fullUrl = tabs[0].url;
+
+    //check if the current page is a Wikipedia article page
+    if (
+      fullUrl.substr(0, 30) == "https://en.wikipedia.org/wiki/" &&
+      fullUrl != "https://en.wikipedia.org/wiki/Main_Page"
+    ) {
+      //do if page is Wikipedia article
+      alert("Yes, its a Wikipedia article.");
+    } else {
+      //do if page is NOT Wikipedia article
+      alert("No. This page is not a Wikipedia article.");
+    }
+  });
+};
