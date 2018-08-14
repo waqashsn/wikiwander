b6ff3941bb5967b77aa50ceef64c6ca68931c01f
Waqas Hassan <waqashsn@yahoo.com>
2018-08-14 21:32:27 +0500

    Added functionality so that extension button is disabled if current page is not Wikipedia article

---
+++ js/content.js
@@ -0,0 +1,12 @@
+var url = document.location.href;
+//check if current tab is not a Wikipedia article page
+if (url.indexOf("//en.wikipedia.org/wiki") === -1 || url == "https://en.wikipedia.org/wiki/Main_Page") {
+  //update and send inactive icons
+  alert(url);
+  browser.runtime.sendMessage({
+    iconPath20: "/images/wikiwand_inactive20.png",
+    iconPath40: "/images/wikiwand_inactive40.png",
+    iconPath25: "/images/wikiwand_inactive25.png",
+    iconPath48: "/images/wikiwand_inactive48.png"
+  });
+}
