dfd6d645a075dd5bf90454d7456bdc006973a493
Waqas Hassan <waqashsn@yahoo.com>
2018-08-14 13:03:13 +0500

    Added functionality to enable read article button only if current tab is a Wikipedia article page. Code refactored to achieve this.

--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,21 @@
 //get the read article btn
 let readBtn = document.getElementById("read_article");
 
-readBtn.onclick = function() {
-  //get currently active tab in browser window
-  browser.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-    //tabs object contains info about active tab
-
-    //get the full url of the tab
+//get url of current tab
+browser.tabs.query({ currentWindow: true, active: true}, function(tabs){
     var fullUrl = tabs[0].url;
-
-    //check if the current page is a Wikipedia article page
-    if (
-      fullUrl.substr(0, 30) == "https://en.wikipedia.org/wiki/" &&
-      fullUrl != "https://en.wikipedia.org/wiki/Main_Page"
-    ) {
-      //do if page is Wikipedia article
-      //extract Wikipedia article topic from url
-      articleTopic = fullUrl.substring(30);
-      
-      //navigate the current tab to topic page on Wikiwand
-      browser.tabs.update({
-          url: "https://www.wikiwand.com/en/" + articleTopic
-      });
-      
+    
+    //check if the page is a Wikipedia article
+    if(fullUrl.substr(0, 30) == "https://en.wikipedia.org/wiki/" && fullUrl != "https://en.wikipedia.org/wiki/Main_Page"){
+        //assign click event to button to open Wikiwand article in current tab
+        readBtn.onclick = function(){
+            articleTopic = fullUrl.substring(30);
+            browser.tabs.update({
+                url: "https://www.wikiwand.com/en/" + articleTopic            
+            });
+        }
     } else {
-      //do if page is NOT Wikipedia article
-      alert("No. This page is not a Wikipedia article.");
+        //disbale the read article button if not a Wikipedia article page
+        readBtn.disabled = true;
     }
-  });
-};
+});
