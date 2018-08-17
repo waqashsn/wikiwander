09e2bef8b05cd4acc2f6bc68454d2a66faa10a25
Waqas Hassan <waqashsn@yahoo.com>
2018-08-17 21:42:08 +0500

    Added basic support for multiple languages.

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 var url = document.location.href;
 //check if current tab is not a Wikipedia article page
-if (url.indexOf("//en.wikipedia.org/wiki") === -1 || url == "https://en.wikipedia.org/wiki/Main_Page") {
+if (url.indexOf("wikipedia.org/wiki") === -1) {
   //update and send inactive icons
   browser.runtime.sendMessage({
     iconPath20: "/images/wikiwand_inactive20.png",
--- js/popup.js
+++ js/popup.js
@@ -6,12 +6,12 @@ browser.tabs.query({ currentWindow: true, active: true}, function(tabs){
     var fullUrl = tabs[0].url;
     
     //check if the page is a Wikipedia article
-    if(fullUrl.substr(0, 30) == "https://en.wikipedia.org/wiki/" && fullUrl != "https://en.wikipedia.org/wiki/Main_Page"){
+    if(fullUrl.substr(11, 19) == "wikipedia.org/wiki/"){
         //assign click event to button to open Wikiwand article in current tab
         readBtn.onclick = function(){
             articleTopic = fullUrl.substring(30);
             browser.tabs.update({
-                url: "https://www.wikiwand.com/en/" + articleTopic            
+                url: "https://www.wikiwand.com/"+ fullUrl.substr(8,2) +"/"+ articleTopic            
             });
         }
     } else {
